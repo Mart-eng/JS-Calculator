01c169f28139fc382b0c429b984fbf8aad200356
213412341345234523452352345234342@users.noreply.github.com <74334103+Mart-eng@users.noreply.github.com>
2021-12-05 21:00:31 -0500

    trying to get output from buttons

--- Calculator.js
+++ Calculator.js
@@ -1,31 +1,31 @@
 // Setting up variables to access each button
 
 var output = document.getElementById("output");
-var addSign = document.getElementById("plus-sign");
-var minusSign = document.getElementById("minus-sign");
-var multiplySign = document.getElementById("multiply-sign");
-var divideSign = document.getElementById("divide-sign");
-var equalSign = document.getElementById("equal-sign");
-var clear = document.getElementById("clear");
-var dot = document.getElementById("dot");
-var zero = document.getElementById("zero");
-var one = document.getElementById("one");
-var two = document.getElementById("two");
-var three = document.getElementById("three");
-var four = document.getElementById("four");
-var five = document.getElementById("five");
-var six = document.getElementById("six");
-var seven = document.getElementById("seven");
-var eight = document.getElementById("eight");
-var nine = document.getElementById("nine");
+var addSign = document.getElementById("plus-sign").addEventListener("click", buttonClicked);
+var minusSign = document.getElementById("minus-sign").addEventListener("click", buttonClicked);
+var multiplySign = document.getElementById("multiply-sign").addEventListener("click", buttonClicked);
+var divideSign = document.getElementById("divide-sign").addEventListener("click", buttonClicked);
+var equalSign = document.getElementById("equal-sign").addEventListener("click", buttonClicked);
+var clear = document.getElementById("clear").addEventListener("click", buttonClicked);
+var dot = document.getElementById("dot").addEventListener("click", buttonClicked);
+var zero = document.getElementById("zero").addEventListener("click", buttonClicked);
+var one = document.getElementById("one").addEventListener("click", buttonClicked);
+var two = document.getElementById("two").addEventListener("click", buttonClicked);
+var three = document.getElementById("three").addEventListener("click", buttonClicked);
+var four = document.getElementById("four").addEventListener("click", buttonClicked);
+var five = document.getElementById("five").addEventListener("click", buttonClicked);
+var six = document.getElementById("six").addEventListener("click", buttonClicked);
+var seven = document.getElementById("seven").addEventListener("click", buttonClicked);
+var eight = document.getElementById("eight").addEventListener("click", buttonClicked);
+var nine = document.getElementById("nine").addEventListener("click", buttonClicked);
 
-const btns = document.querySelectorAll('button');
-
-console.log(btns);
-btns.forEach(btn => { 
-    btn.addEventListener("click", buttonClicked());
-});
 
 function buttonClicked() {
-    
+    console.log(this.id);
+    switch(Event.getElementById) {
+        case addSign:
+            console.log("hi");
+
+    }
+    event.preventDefault();
 }
